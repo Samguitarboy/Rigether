99f063bc1c4f8135456b93033732c35c6dc4c8fd
Samguitarboy <sam65ocean@gmail.com>
2018-06-18 01:05:43 +0800

    S2T(4)

--- wooaccounting/src/main/java/com/wooaccounting/views/AccountingPresenter.java
+++ wooaccounting/src/main/java/com/wooaccounting/views/AccountingPresenter.java
@@ -12,6 +12,19 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import net.sourceforge.javaflacencoder.FLACFileWriter;
 
+import com.google.cloud.speech.v1p1beta1.RecognitionAudio;
+import com.google.cloud.speech.v1p1beta1.RecognitionConfig;
+import com.google.cloud.speech.v1p1beta1.RecognitionConfig.AudioEncoding;
+import com.google.cloud.speech.v1p1beta1.RecognizeResponse;
+import com.google.cloud.speech.v1p1beta1.SpeechClient;
+import com.google.cloud.speech.v1p1beta1.SpeechRecognitionAlternative;
+import com.google.cloud.speech.v1p1beta1.SpeechRecognitionResult;
+import com.google.protobuf.ByteString;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class AccountingPresenter {
 
     @FXML
@@ -52,7 +65,7 @@ public class AccountingPresenter {
         
         System.out.println("8888");
         try {
-		mic.captureAudioToFile("./record/sound.wav");
+		mic.captureAudioToFile("./record/sound.flac");
 	} catch (Exception ex) {
 		ex.printStackTrace();
 	}
@@ -65,7 +78,39 @@ public class AccountingPresenter {
     }
     private void stopClick(Microphone mic)  throws Exception  {
        mic.close();
+           // Instantiates a client
+    try (SpeechClient speechClient = SpeechClient.create()) {
 
+      // The path to the audio file to transcribe
+      String fileName = "./record/sound.flac";
+
+      // Reads the audio file into memory
+      Path path = Paths.get(fileName);
+      byte[] data = Files.readAllBytes(path);
+      ByteString audioBytes = ByteString.copyFrom(data);
+
+      // Builds the sync recognize request
+      RecognitionConfig config = RecognitionConfig.newBuilder()
+          .setEncoding(AudioEncoding.FLAC)
+          .setSampleRateHertz(16000)
+          .setLanguageCode("cmn-Hant-TW")
+          .build();
+      RecognitionAudio audio = RecognitionAudio.newBuilder()
+          .setContent(audioBytes)
+          .build();
+
+      // Performs speech recognition on the audio file
+      RecognizeResponse response = speechClient.recognize(config, audio);
+      List<SpeechRecognitionResult> results = response.getResultsList();
+
+      for (SpeechRecognitionResult result : results) {
+        // There can be several alternative transcripts for a given chunk of speech. Just use the
+        // first (most likely) one here.
+        SpeechRecognitionAlternative alternative = result.getAlternativesList().get(0);
+        System.out.printf("Transcription: %s%n", alternative.getTranscript());
+        speech2text.setText(alternative.getTranscript());
+      }
+    }
        record.setDisable(false);
        record.setVisible(true);
        stop.setDisable(true);
