27b124d92106b89ca44f2689c2f65b7587d22e65
Samguitarboy <sam65ocean@gmail.com>
2018-06-17 15:49:00 +0800

    S2T(1)

--- wooaccounting/src/main/java/com/wooaccounting/Wooaccounting.java
+++ wooaccounting/src/main/java/com/wooaccounting/Wooaccounting.java
@@ -32,4 +32,6 @@ public class Wooaccounting extends MobileApplication {
     }
     
     
+    
+    
 }
--- wooaccounting/src/main/java/com/wooaccounting/views/AccountingPresenter.java
+++ wooaccounting/src/main/java/com/wooaccounting/views/AccountingPresenter.java
@@ -1,14 +1,18 @@
 package com.wooaccounting.views;
 
+import com.darkprograms.speech.microphone.Microphone;
+import com.darkprograms.speech.recognizer.GSpeechDuplex;
 import com.gluonhq.charm.glisten.animation.BounceInRightTransition;
 import com.gluonhq.charm.glisten.application.MobileApplication;
 import com.gluonhq.charm.glisten.control.AppBar;
-import com.gluonhq.charm.glisten.layout.layer.FloatingActionButton;
 import com.gluonhq.charm.glisten.mvc.View;
 import com.gluonhq.charm.glisten.visual.MaterialDesignIcon;
 import com.wooaccounting.Wooaccounting;
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import net.sourceforge.javaflacencoder.FLACFileWriter;
 
 public class AccountingPresenter {
 
@@ -16,6 +20,8 @@ public class AccountingPresenter {
     private View accounting;
     @FXML
     private Label speech2text;
+    @FXML
+    private Button record;
     
     public void initialize() {
         accounting.setShowTransitionFactory(BounceInRightTransition::new);
@@ -30,10 +36,20 @@ public class AccountingPresenter {
                         System.out.println("Favorite")));
             }
         });
+        
+        record.setOnAction(e->{buttonClick();});
+        
     }
     
-    @FXML
-    void buttonClick() {
+    private void buttonClick() {
+        
+        Microphone mic = new Microphone(FLACFileWriter.FLAC);
+        GSpeechDuplex duplex = new GSpeechDuplex("AIzaSyD8W-lKEyrwGngsu0-XP1_ztyINGHPGHLw");
+        duplex.setLanguage("zh-TW");
+        System.out.println("HI");
+        record.setDisable(true);
         speech2text.setText("YO Man ~");
+        
+        
     }
 }
