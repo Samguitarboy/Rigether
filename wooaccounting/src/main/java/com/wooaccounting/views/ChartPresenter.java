b74a586fd470c00f0be6a0cb62c1475e7828adf1
s1031416 <s091139@gmail.com>
2018-06-23 21:45:55 +0800

    piechart testing

--- wooaccounting/src/main/java/com/wooaccounting/views/ChartPresenter.java
+++ wooaccounting/src/main/java/com/wooaccounting/views/ChartPresenter.java
@@ -6,7 +6,10 @@ import com.gluonhq.charm.glisten.control.AppBar;
 import com.gluonhq.charm.glisten.mvc.View;
 import com.gluonhq.charm.glisten.visual.MaterialDesignIcon;
 import com.wooaccounting.Wooaccounting;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.chart.PieChart;
 import javafx.scene.control.Label;
 
 public class ChartPresenter {
@@ -15,10 +18,20 @@ public class ChartPresenter {
     private View chart;
     @FXML
     private Label label;
+    @FXML
+    private PieChart piechart;
     
+
     public void initialize() {
-        chart.setShowTransitionFactory(BounceInRightTransition::new);
         
+        ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList(
+                new PieChart.Data("娛樂", 20),
+                new PieChart.Data("飲食", 40),
+                new PieChart.Data("其他", 30),
+                new PieChart.Data("交通", 10)
+        );
+        piechart.setData(pieChartData);
+        chart.setShowTransitionFactory(BounceInRightTransition::new);
         chart.showingProperty().addListener((obs, oldValue, newValue) -> {
             if (newValue) {
                 AppBar appBar = MobileApplication.getInstance().getAppBar();
