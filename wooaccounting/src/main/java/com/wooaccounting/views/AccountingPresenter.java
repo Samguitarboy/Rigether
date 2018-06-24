324acacf015d5a5fcd33f930475cedb1ce0403b1
samguitarboy <sam65ocean@gmail.com>
2018-06-24 18:45:45 +0800

    accounting label background change

--- wooaccounting/src/main/java/com/wooaccounting/views/AccountingPresenter.java
+++ wooaccounting/src/main/java/com/wooaccounting/views/AccountingPresenter.java
@@ -11,7 +11,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import net.sourceforge.javaflacencoder.FLACFileWriter;
-
+import javafx.scene.layout.VBox;
 import com.google.cloud.speech.v1p1beta1.RecognitionAudio;
 import com.google.cloud.speech.v1p1beta1.RecognitionConfig;
 import com.google.cloud.speech.v1p1beta1.RecognitionConfig.AudioEncoding;
@@ -25,7 +25,10 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
 public class AccountingPresenter {
 
@@ -48,8 +51,12 @@ public class AccountingPresenter {
     @FXML
     private Button upload;
 
+    @FXML
+    private AnchorPane wordpane;
+
     private String origintext;
     private int choosenum = 0;
+    private int count = 1;
 
     public void initialize() {
         accounting.setShowTransitionFactory(BounceInRightTransition::new);
@@ -104,12 +111,7 @@ public class AccountingPresenter {
         insertdb.recordtoDB(origintext, target, Integer.parseInt(number));
         whatyousay.setText("What You Say");
         speech2text.setPromptText("");
-        upload.setDisable(true);
-        upload.setVisible(false);
-        edit.setDisable(true);
-        edit.setVisible(false);
-        enter.setDisable(true);
-        enter.setVisible(false);
+
     }
 
     private void changetext() {
@@ -140,7 +142,10 @@ public class AccountingPresenter {
             ex.printStackTrace();
         }
         System.out.println("YES~");
-
+        record.setDisable(true);
+        record.setVisible(false);
+        stop.setDisable(false);
+        stop.setVisible(true);
 
     }
 
@@ -177,24 +182,22 @@ public class AccountingPresenter {
                 SpeechRecognitionAlternative alternative = result.getAlternativesList().get(0);
                 System.out.printf("Transcription: %s%n", alternative.getTranscript());
 
-                if (0 == "1".compareTo(alternative.getTranscript())) {
-                    
-                }
-                else if (0 == "2".compareTo(alternative.getTranscript())) {
+                whatyousay.setDisable(false);
+                whatyousay.setVisible(true);
+                
+                VBox v = new VBox();
+                v.setAlignment(Pos.CENTER_RIGHT);
+                v.setPrefSize(330, 47);
+                v.setLayoutY(46 * count -37);
+                count++;
+                Label newtext = new Label("我說1,我要記錄支出 !");
+                newtext.setPrefSize(175, 37);
+                newtext.setAlignment(Pos.CENTER_RIGHT);
+                newtext.setPadding(new Insets(20));
+                newtext.setStyle("-fx-background-image: url(\"/com/wooaccounting/views/label1.png\");");
+                v.getChildren().add(newtext);
+                wordpane.getChildren().add(0, v);
 
-                }
-                else if (0 == "3".compareTo(alternative.getTranscript())) {
-
-                }
-                else{
-                    origintext = alternative.getTranscript();
-
-                    whatyousay.setText(alternative.getTranscript().replace("我", "").replace("今天", "").replace("吃", "").replace("早上", "").replace("中午", "").replace("晚上", "")
-                            .replace("塊", "").replace("元", "").replace("昨天", "").replace("是", "").replace("花", "").replace("了", ""));
-
-                    speech2text.setPromptText(alternative.getTranscript().replace("我", "").replace("今天", "").replace("吃", "").replace("早上", "").replace("中午", "").replace("晚上", "")
-                            .replace("塊", "").replace("元", "").replace("昨天", "").replace("是", "").replace("花", "").replace("了", ""));
-                }
             }
         }
 
