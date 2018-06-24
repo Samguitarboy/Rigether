3b30b33057164611d67743d8ccd53d7cca2a2f29
unknown <chuanhsiang>
2018-06-24 17:12:20 +0800

    calendar

--- wooaccounting/src/main/java/com/wooaccounting/views/CalendarPresenter.java
+++ wooaccounting/src/main/java/com/wooaccounting/views/CalendarPresenter.java
@@ -7,15 +7,22 @@ import com.gluonhq.charm.glisten.mvc.View;
 import com.gluonhq.charm.glisten.visual.MaterialDesignIcon;
 import com.wooaccounting.Wooaccounting;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import java.time.LocalDate;
 import javafx.scene.control.Label;
+import com.wooaccounting.GetdataSQL;
+import javafx.scene.control.DatePicker;
 
 public class CalendarPresenter {
 
     @FXML
     private View calendar;
     @FXML
+    private Button btnSearch;
+    @FXML
     private Label label;
-    
+    @FXML
+    private DatePicker datepicker;
     public void initialize() {
         calendar.setShowTransitionFactory(BounceInRightTransition::new);
         
@@ -27,7 +34,16 @@ public class CalendarPresenter {
                 appBar.setTitleText("Calendar");
             }
         });
+        
+        btnSearch.setOnAction(e->{
+            GetdataSQL d = new GetdataSQL();
+            LocalDate select = datepicker.getValue();
+            label.setText(d.searchdate(select.toString()));
+        });
+        
+        datepicker.setValue(LocalDate.now());
     }
+    
     
     
 }
