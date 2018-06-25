da3334b43352669f8139292652f5c1985070e785
samguitarboy <sam65ocean@gmail.com>
2018-06-26 00:03:36 +0800

    Wallet data sync db

--- wooaccounting/src/main/java/com/wooaccounting/GetdataSQL.java
+++ wooaccounting/src/main/java/com/wooaccounting/GetdataSQL.java
@@ -1,68 +1,68 @@
 package com.wooaccounting;
 
 public class GetdataSQL {
-    public String classify(String input){
-        String result="其他";
+
+    public String classify(String input) {
+        String result = "其他";
         config con = new config();
         String connectionStr = "jdbc:mysql://" + con.getUrlstr() + "/" + con.getDBName() + "?user=" + con.getUserstr() + "&password=" + con.getPw();
         String querydata = "select foodname from Food";
         MySQLConnector MSC_query = new MySQLConnector();
         MSC_query.connectDB(connectionStr);
         MSC_query.clearresult();
-        
+
         MSC_query.doquery(querydata);
         //    if(MSC_query.getResult().toString()==input)
-        if (MSC_query.getResult().toString().contains(input)){
+        if (MSC_query.getResult().toString().contains(input)) {
             result = "飲食";
             System.out.println("it is a food!!");
             MSC_query.clearresult();
             return result;
         }
-       querydata = "select entertainmentname from Entertainment";
-       MSC_query.doquery(querydata);
+        querydata = "select entertainmentname from Entertainment";
+        MSC_query.doquery(querydata);
         //    if(MSC_query.getResult().toString()==input)
-       if (MSC_query.getResult().toString().contains(input)){
+        if (MSC_query.getResult().toString().contains(input)) {
             result = "娛樂";
             System.out.println("it is not a food!!");
             MSC_query.clearresult();
             return result;
-       }
-       querydata = "select trafficname from Traffic";
-       MSC_query.doquery(querydata);
+        }
+        querydata = "select trafficname from Traffic";
+        MSC_query.doquery(querydata);
         //    if(MSC_query.getResult().toString()==input)
-       if (MSC_query.getResult().toString().contains(input)){
+        if (MSC_query.getResult().toString().contains(input)) {
             result = "交通";
             System.out.println("it is not a food!!");
             MSC_query.clearresult();
             return result;
-       }
-       
-       querydata = "select clothname from Cloth";
-       MSC_query.doquery(querydata);
+        }
+
+        querydata = "select clothname from Cloth";
+        MSC_query.doquery(querydata);
         //    if(MSC_query.getResult().toString()==input)
-       if (MSC_query.getResult().toString().contains(input)){
+        if (MSC_query.getResult().toString().contains(input)) {
             result = "服飾";
             System.out.println("it is not a food!!");
             MSC_query.clearresult();
             return result;
-       }
-       querydata = "select livingname from Living";
-       MSC_query.doquery(querydata);
+        }
+        querydata = "select livingname from Living";
+        MSC_query.doquery(querydata);
         //    if(MSC_query.getResult().toString()==input)
-       if (MSC_query.getResult().toString().contains(input)){
+        if (MSC_query.getResult().toString().contains(input)) {
             result = "住宅";
             System.out.println("it is not a food!!");
             MSC_query.clearresult();
             return result;
-       }
-            //returndata += MSC_query.getResult().toString().split("\n")[i]+"\n";
+        }
+        //returndata += MSC_query.getResult().toString().split("\n")[i]+"\n";
         MSC_query.clearresult();
         return result;
-        
-    
+
     }
-    
-    public String searchall(){
+
+    public String searchall() {
         config con = new config();
         String connectionStr = "jdbc:mysql://" + con.getUrlstr() + "/" + con.getDBName() + "?user=" + con.getUserstr() + "&password=" + con.getPw();
         String querynum = "select count(*) from Accounting";
@@ -70,66 +70,98 @@ public class GetdataSQL {
         String returndata = "";
         MySQLConnector MSC_query = new MySQLConnector();
         MSC_query.connectDB(connectionStr);
-        
+
         MSC_query.doquery(querynum);
         int count = Integer.valueOf(MSC_query.getResult().toString().trim());
         MSC_query.clearresult();
-        
+
         MSC_query.doquery(querydata);
-        for(int i=0;i < count;i++)
-            returndata += MSC_query.getResult().toString().split("\n")[i]+"\n";
+        for (int i = 0; i < count; i++) {
+            returndata += MSC_query.getResult().toString().split("\n")[i] + "\n";
+        }
 
         MSC_query.clearresult();
-        return  returndata;
+        return returndata;
     }
-    public String searchdate(String date){
+
+    public String searchdate(String date) {
         config con = new config();
         String connectionStr = "jdbc:mysql://" + con.getUrlstr() + "/" + con.getDBName() + "?user=" + con.getUserstr() + "&password=" + con.getPw();
-        String querynum = "select count(*) from Accounting where recordDate ='"+date+"'";
-        String querydate = "select target,price from Accounting where recordDate ='"+date+"'";
-        String querytime = "select modifiedTime from Accounting where recordDate ='"+date+"'";
+        String querynum = "select count(*) from Accounting where recordDate ='" + date + "'";
+        String querydate = "select target,price from Accounting where recordDate ='" + date + "'";
+        String querytime = "select modifiedTime from Accounting where recordDate ='" + date + "'";
         String returndata = "";
         MySQLConnector MSC_query = new MySQLConnector();
         MSC_query.connectDB(connectionStr);
-        
+
         MSC_query.doquery(querynum);
         int count = Integer.valueOf(MSC_query.getResult().toString().trim());
         MSC_query.clearresult();
-        
+
         MSC_query.doquery(querydate);
-        
-        
+
         MySQLConnector MSC_query2 = new MySQLConnector();
-        MSC_query2.connectDB(connectionStr);       
+        MSC_query2.connectDB(connectionStr);
         MSC_query2.doquery(querytime);
-               
-        for(int i=0;i < count;i++){
-            String str =  MSC_query2.getResult().toString().split("\n")[i].substring(10);
-            String str2 = str.substring(0,str.length()-3);
-            returndata += MSC_query.getResult().toString().split("\n")[i]+ str2 +"\n";
+
+        for (int i = 0; i < count; i++) {
+            String str = MSC_query2.getResult().toString().split("\n")[i].substring(10);
+            String str2 = str.substring(0, str.length() - 3);
+            returndata += MSC_query.getResult().toString().split("\n")[i] + str2 + "\n";
         }
         MSC_query.clearresult();
         MSC_query2.clearresult();
-        return  returndata;
+        return returndata;
     }
-    public int getsurplus(){
+
+    public int getsurplus() {
         config con = new config();
         String connectionStr = "jdbc:mysql://" + con.getUrlstr() + "/" + con.getDBName() + "?user=" + con.getUserstr() + "&password=" + con.getPw();
         String querysumofexpenditure = "select sum(price) from Accounting";
         String querysumofincome = "select sum(money) from income";
-        String returndata = "";
+
         MySQLConnector MSC_query = new MySQLConnector();
         MSC_query.connectDB(connectionStr);
-        
+
         MSC_query.doquery(querysumofexpenditure);
         int expenditure = Integer.valueOf(MSC_query.getResult().toString().trim());
         MSC_query.clearresult();
-        
+
         MSC_query.doquery(querysumofincome);
         int income = Integer.valueOf(MSC_query.getResult().toString().trim());
         MSC_query.clearresult();
-        
+
         int surplus = income - expenditure;
-        return  surplus;
+        return surplus;
+    }
+
+    public int getincome() {
+        config con = new config();
+        String connectionStr = "jdbc:mysql://" + con.getUrlstr() + "/" + con.getDBName() + "?user=" + con.getUserstr() + "&password=" + con.getPw();
+        String querysumofincome = "select sum(money) from income";
+
+        MySQLConnector MSC_query = new MySQLConnector();
+        MSC_query.connectDB(connectionStr);
+
+        MSC_query.doquery(querysumofincome);
+        int income = Integer.valueOf(MSC_query.getResult().toString().trim());
+        MSC_query.clearresult();
+
+        return income;
+    }
+
+    public int getexpenditure() {
+        config con = new config();
+        String connectionStr = "jdbc:mysql://" + con.getUrlstr() + "/" + con.getDBName() + "?user=" + con.getUserstr() + "&password=" + con.getPw();
+        String querysumofexpenditure = "select sum(price) from Accounting";
+
+        MySQLConnector MSC_query = new MySQLConnector();
+        MSC_query.connectDB(connectionStr);
+
+        MSC_query.doquery(querysumofexpenditure);
+        int expenditure = Integer.valueOf(MSC_query.getResult().toString().trim());
+        MSC_query.clearresult();
+
+        return expenditure;
     }
 }
--- wooaccounting/src/main/java/com/wooaccounting/views/WalletPresenter.java
+++ wooaccounting/src/main/java/com/wooaccounting/views/WalletPresenter.java
@@ -5,35 +5,38 @@ import com.gluonhq.charm.glisten.application.MobileApplication;
 import com.gluonhq.charm.glisten.control.AppBar;
 import com.gluonhq.charm.glisten.mvc.View;
 import com.gluonhq.charm.glisten.visual.MaterialDesignIcon;
+import com.wooaccounting.GetdataSQL;
 import com.wooaccounting.Wooaccounting;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-
 
 public class WalletPresenter {
 
     @FXML
     private View wallet;
     @FXML
-    private Label deposit, expend, balance;
-    
+    private Label income, expend, balance;
+
     public void initialize() {
-        int deposit_value=30000, expend_value =11200,balance_value = deposit_value-expend_value;
-        
-        deposit.setText(deposit.getText()+Integer.toString(deposit_value));
-        expend.setText(expend.getText()+Integer.toString(expend_value));
-        balance.setText(balance.getText()+Integer.toString(balance_value));
+        GetdataSQL show = new GetdataSQL();
+
+        int income_value = show.getincome();
+        int expend_value = show.getexpenditure();
+        int balance_value = income_value - expend_value;
+
+        income.setText(income.getText() + Integer.toString(income_value));
+        expend.setText(expend.getText() + Integer.toString(expend_value));
+        balance.setText(balance.getText() + Integer.toString(balance_value));
         wallet.setShowTransitionFactory(BounceInRightTransition::new);
-        
+
         wallet.showingProperty().addListener((obs, oldValue, newValue) -> {
             if (newValue) {
                 AppBar appBar = MobileApplication.getInstance().getAppBar();
-                appBar.setNavIcon(MaterialDesignIcon.MENU.button(e -> 
-                        MobileApplication.getInstance().showLayer(Wooaccounting.MENU_LAYER)));
+                appBar.setNavIcon(MaterialDesignIcon.MENU.button(e
+                        -> MobileApplication.getInstance().showLayer(Wooaccounting.MENU_LAYER)));
                 appBar.setTitleText("Wallet");
             }
         });
     }
-    
-    
+
 }
